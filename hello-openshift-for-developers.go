f7ccfa3c1e0855a01f1764fe92d799f734c0cd29
dnTmax <111535912+dnTmax@users.noreply.github.com>
2023-10-17 11:29:09 +0200

    Update hello-openshift-for-developers.go
    jkk

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello ensam formation"
+		response = "Hello ensam formation yHHrp-MTqrn-uXnsg-nEKSQ "
 	}
 
 	fmt.Fprintln(w, response)
